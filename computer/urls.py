00f90ed70a49b9aeb6cc70286ca72ace3aef3140
prkspkrl <rockpokhrel@gmail.com>
2023-07-28 14:56:18 +0545

    fifth commit

--- computer/urls.py
+++ computer/urls.py
@@ -17,10 +17,16 @@ urlpatterns = [
 
     path('brand', views.CreateBrandView.as_view(), name='brand'),
     path('brandlist', views.ListBrandView.as_view(), name='listbrand'),
+    path('brand/update/<int:pk>', views.UpdateBrandView.as_view(), name='update_brand'),
+    path('brand/delete/<int:pk>', views.DeleteBrandView.as_view(), name='delete_brand'),
 
     path('generation', views.CreateGenerationView.as_view(), name='generation'),
     path('generationlist', views.ListGenerationView.as_view(), name='listgeneration'),
+    path('generation/update/<int:pk>', views.UpdateGenerationView.as_view(), name='update_generation'),
+    path('generation/delete/<int:pk>', views.DeleteGenerationView.as_view(), name='delete_generation'),
 
     path('specification', views.CreateSpecificationView.as_view(), name='specification'),
     path('specificationlist', views.ListSpecificationView.as_view(), name='listspecification'),
+    path('specification/update/<int:pk>', views.UpdateSpecificationView.as_view(), name='update_specification'),
+    path('specification/delete/<int:pk>', views.DeleteSpecificationView.as_view(), name='specification_brand'),
 ]
--- computer/views.py
+++ computer/views.py
@@ -1,104 +1,9 @@
-# from .forms import ComputerForm, BrandForm, SpecificationForm, GenerationForm
-# from django.shortcuts import render, get_object_or_404, redirect
-# from django.http import HttpResponse
-# from .models import Computer
-#
-# # Create your views here.
-# def home(request):
-#     list = Computer.objects.all()
-#     context = {
-#         'list':list
-#     }
-#     return render(request, 'home.html', context)
-#
-# def create_computer(request):
-#     if request.method == 'POST':
-#         form = ComputerForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponse('submitted')
-#
-#     else:
-#         form = ComputerForm()
-#         context = {
-#             'form': form,
-#         }
-#         return render(request, 'computerform.html', context)
-#
-#
-# def update_computer(request,pk):
-#     computer = get_object_or_404(Computer, pk=pk)
-#     if request.method == 'POST':
-#         form = ComputerForm(request.POST, instance=computer)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('/')
-#
-#     else:
-#         form = ComputerForm(instance=computer)
-#         return render(request, 'update.html', {'form': form})
-#
-# def delete_computer(request,pk):
-#     computer = get_object_or_404(Computer, pk=pk)
-#     if request.method == 'POST':
-#         print('abc')
-#         computer.delete()
-#         return redirect('/')
-#     else:
-#         computer.delete()
-#         return redirect('/')
-#
-#
-# def create_brand(request):
-#     if request.method == 'POST':
-#         form = BrandForm(request.POST, request.FILES)
-#         if form.is_valid():
-#
-#             form.save()
-#             return redirect('/')
-#     else:
-#         form = BrandForm()
-#         context = {
-#             'form': form,
-#         }
-#         return render(request, 'brandform.html', context)
-#
-#
-# def create_generation(request):
-#     if request.method == 'POST':
-#         form = GenerationForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('/')
-#
-#     else:
-#         form = GenerationForm()
-#         context = {
-#             'form': form,
-#         }
-#         return render(request, 'generationform.html', context)
-#
-#
-# def create_specification(request):
-#     if request.method == 'POST':
-#         form = SpecificationForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('/')
-#
-#     else:
-#         form = SpecificationForm()
-#         context = {
-#             'form': form,
-#         }
-#         return render(request, 'specificationform.html', context)
-
-
 from .forms import ComputerForm, BrandForm, SpecificationForm, GenerationForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import Computer, ComputerBrands,ComputerGeneration, ComputerSpecification
+from django.http import JsonResponse
 
 class HomeView(View):
     def get(self, request):
@@ -107,15 +12,6 @@ class HomeView(View):
             'list': list
         }
         return render(request, 'home.html', context)
-
-class ListBrandView(View):
-    def get(self,request):
-        list = ComputerBrands.objects.all()
-        context = {
-            'list': list,
-        }
-        return render(request,'brandlist.html', context)
-
 class CreateComputerView(View):
     def get(self, request):
         form = ComputerForm()
@@ -138,21 +34,40 @@ class UpdateComputerView(View):
 
     def post(self, request, pk):
         computer = get_object_or_404(Computer, pk=pk)
-        form = ComputerForm(request.POST, instance=computer)
+        form = ComputerForm(request.POST, request.FILES, instance=computer)
+        print(form)
         if form.is_valid():
+
             form.save()
             return redirect('/')
 
-class DeleteComputerView(View):
-    def get(self, request, pk):
-        computer = get_object_or_404(Computer, pk=pk)
-        computer.delete()
-        return redirect('/')
+# class DeleteComputerView(View):
+#     def get(self, request, pk):
+#         computer = get_object_or_404(Computer, pk=pk)
+#         computer.delete()
+#         return redirect('/')
+#
+#     def post(self, request, pk):
+#         computer = get_object_or_404(Computer, pk=pk)
+#         computer.delete()
+#         return redirect('/')
 
+
+class DeleteComputerView(View):
     def post(self, request, pk):
         computer = get_object_or_404(Computer, pk=pk)
+        computer_name = computer.computer_code
         computer.delete()
-        return redirect('/')
+        return JsonResponse({'message': f'Generation "{computer_name}" deleted successfully.'})
+
+
+class ListBrandView(View):
+    def get(self,request):
+        list = ComputerBrands.objects.all()
+        context = {
+            'list': list,
+        }
+        return render(request,'brandlist.html', context)
 
 class CreateBrandView(View):
     def get(self, request):
@@ -166,7 +81,25 @@ class CreateBrandView(View):
         form = BrandForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('listbrand')
+class UpdateBrandView(View):
+    def get(self, request, pk):
+        brand = get_object_or_404(ComputerBrands, pk=pk)
+        form = BrandForm(instance=brand)
+        return render(request, 'brand_update.html', {'form': form})
+    def post(self, request, pk):
+        brand = get_object_or_404(ComputerBrands, pk=pk)
+        form = BrandForm(request.POST,request.FILES, instance=brand)
+        if form.is_valid():
+            form.save()
+            return redirect('listbrand')
+
+class DeleteBrandView(View):
+    def post(self, request, pk):
+        brand = get_object_or_404(ComputerBrands, pk=pk)
+        brand_name = brand.brand_name
+        brand.delete()
+        return JsonResponse({'message': f'Generation "{brand_name}" deleted successfully.'})
 
 class CreateGenerationView(View):
     def get(self, request):
@@ -180,7 +113,7 @@ class CreateGenerationView(View):
         form = GenerationForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('listgeneration')
 
 class ListGenerationView(View):
     def get(self, request):
@@ -189,6 +122,26 @@ class ListGenerationView(View):
             'list':list,
         }
         return render(request, 'generationlist.html', context)
+
+class DeleteGenerationView(View):
+    def post(self, request, pk):
+        generation = get_object_or_404(ComputerGeneration, pk=pk)
+        generation_name = generation.generation
+        generation.delete()
+        return JsonResponse({'message': f'Generation "{generation_name}" deleted successfully.'})
+
+class UpdateGenerationView(View):
+    def get(self, request, pk):
+        generation = get_object_or_404(ComputerGeneration, pk=pk)
+        form = GenerationForm(instance=generation)
+        return render(request, 'generation_update.html', {'form': form})
+
+    def post(self, request, pk):
+        generation = get_object_or_404(ComputerGeneration, pk=pk)
+        form = GenerationForm(request.POST, instance=generation)
+        if form.is_valid():
+            form.save()
+            return redirect('listgeneration')
 
 class CreateSpecificationView(View):
     def get(self, request):
@@ -202,7 +155,7 @@ class CreateSpecificationView(View):
         form = SpecificationForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('listspecification')
 
 class ListSpecificationView(View):
     def get(self, request):
@@ -211,3 +164,23 @@ class ListSpecificationView(View):
             'list':list,
         }
         return render(request,'specificationlist.html',context)
+
+class DeleteSpecificationView(View):
+    def post(self, request, pk):
+        specification = get_object_or_404(ComputerSpecification, pk=pk)
+        specification_name = specification.generation
+        specification.delete()
+        return JsonResponse({'message': f'Generation "{specification_name}" deleted successfully.'})
+
+class UpdateSpecificationView(View):
+    def get(self, request, pk):
+        specification = get_object_or_404(ComputerSpecification, pk=pk)
+        form = SpecificationForm(instance=specification)
+        return render(request, 'specification_update.html', {'form': form})
+
+    def post(self, request, pk):
+        specification = get_object_or_404(ComputerSpecification, pk=pk)
+        form = SpecificationForm(request.POST, instance=specification)
+        if form.is_valid():
+            form.save()
+            return redirect('listspecification')
